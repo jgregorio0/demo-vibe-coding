e54772e56bbf8fd3c96051d7306374857cfba06b
jesus.gregorio <jesus.gregorio@sngular.com>
2026-05-21 15:19:31 +0200

    fix: fix test dependencies

--- src/test/java/dev/jgregorio/demo/vibe/coding/BaseIT.java
+++ src/test/java/dev/jgregorio/demo/vibe/coding/BaseIT.java
@@ -1,11 +1,13 @@
 package dev.jgregorio.demo.vibe.coding;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 @ActiveProfiles("dev")
 public abstract class BaseIT {
 
